da13d3b4ef040185eea44c0c2cd7bed249d882a2
nigranac <can.argin.16@gmail.com>
2021-01-26 00:57:17 +0300

    first commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+
+} from 'react-native';
+
+const App = () => {
+  return (
+    <View>
+      <Text>Example</Text>
+    </View>
+  );
+};
+
+export default App;
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#FF7B73',
+  }
+});
